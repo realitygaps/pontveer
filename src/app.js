e9371f31f063f85acd4dc5d45045963890eb3f9a
realitygaps <realitygaps@users.noreply.github.com>
2015-06-29 23:09:22 +0200

    Initial commit with most routes

---
+++ src/app.js
@@ -0,0 +1,49 @@
+var UI = require('ui');
+var ajax = require('ajax');
+var dataJSON = [];
+var parseFeed = function(data) {
+	var items = [];
+	
+	for(var i = 0; i < 8; i++) {
+		var location = data[i].location;
+		var destination = data[i].destination;
+		var departuretime = data[i].departures[1].time;
+		var nextdeparturetime = data[i].departures[2].time;
+	
+		
+	//var route = data.route;
+	//var location = data.location;
+	//var destination = data.destination;
+	//var departuretime = data.departures[1].time;
+	//var nextdeparturetime = data.departures[2].time;
+	
+	items.push({
+		title: location.substring(0,4) + "->" + destination.substring(0,4),
+		subtitle: departuretime + "  |  " + nextdeparturetime
+	});
+	}
+  return items;
+};
+  
+ajax({
+    url:'http://gpls.nl/pontveer.php',
+    type:'json'
+  },
+  function(data) {
+	//data = { url: 'http://pontveer.nl/api/?r=NDSM-veer&l=Centraal+Station&d=NDSM-werf', type: 'json'};
+	dataJSON = parseFeed(data);
+    console.log('data');
+    console.log('SHOW MENU');
+
+	var matchMenu = new UI.Menu({
+		sections: [{
+			title: 'Pontveer',
+			items: dataJSON
+		}]
+	});
+	matchMenu.show();
+  },
+  function(error) {
+    console.log('Download failed: ' + error);
+  }
+);
